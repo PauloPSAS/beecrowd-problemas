de7b01a1b45dad2010da614a1250d7e7f78a5791
Paulo Souza <paulosergio.games1997@gmail.com>
2022-04-21 00:24:11 -0300

    Adicionando problemas resolvidos do beecrowd

---
+++ python/1006.py
@@ -0,0 +1,5 @@
+x = float(input())
+y = float(input())
+z = float(input())
+media = ((2 * x) + (3 * y) + (5 * z)) / (2 + 3 + 5)
+print('MEDIA = {:.1f}'.format(media))
---
+++ python/1008.py
@@ -0,0 +1,6 @@
+n = int(input())
+h = int(input())
+sh = float(input())
+s = h * sh
+print('NUMBER =', n)
+print('SALARY = U$ {:.2f}'.format(s))
