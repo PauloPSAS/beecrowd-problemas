9cafb3afa481765f6cf2f057e9fa413913d4fc7f
Paulo Souza <paulosergio.games1997@gmail.com>
2022-04-22 09:18:05 -0300

    novas submissões

---
+++ python/1014.py
@@ -0,0 +1,4 @@
+km = int(input())
+combustivel = float(input())
+kml = km / combustivel
+print('%.3f km/l' % kml)
---
+++ python/1015.py
@@ -0,0 +1,7 @@
+from math import sqrt, pow
+vx = input().split()
+vy = input().split()
+x1, y1 = vx
+x2, y2 = vy
+d = sqrt((pow(float(x2) - float(x1), 2)) + (pow(float(y2) - float(y1), 2)))
+print('%.4f' % d)
