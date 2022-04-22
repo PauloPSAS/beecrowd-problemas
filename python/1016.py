66384b9af73a223dcc668da501a4159c05b90f56
Paulo Souza <paulosergio.games1997@gmail.com>
2022-04-22 09:27:40 -0300

    ...

---
+++ python/1016.py
@@ -0,0 +1,3 @@
+x = int(input())
+min = x * 2
+print(f'{min} minutos')
---
+++ python/1017.py
@@ -0,0 +1,4 @@
+tempo_viagem = float(input())
+velocidade = float(input())
+quilometros_litro = (tempo_viagem * velocidade) / 12
+print(f'{quilometros_litro:.3f}')
