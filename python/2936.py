71453b5dd1215f76fe4b3525f099b95e38fd8546
Paulo Souza <77758720+PauloPSAS@users.noreply.github.com>
2022-08-29 19:51:48 -0300

    2936_Quanta mandioca?

---
+++ python/2936.py
@@ -0,0 +1,15 @@
+chica = 225
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapiguari = int(input())
+iara = int(input())
+
+curupira_porcao = 300 * curupira
+boitata_porcao = 1500 * boitata
+boto_porcao = 600 * boto
+mapiguari_porcao = 1000 * mapiguari
+iara_porcao = 150 * iara
+
+total = chica + curupira_porcao + boitata_porcao + boto_porcao + mapiguari_porcao + iara_porcao
+print(total)
