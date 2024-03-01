b7c363c93ae939342b5fb5561e7a659786eb0078
Paulo <paulosergio.games1997@gmail.com>
2024-03-01 00:12:47 -0300

    Update 1047.py

--- Problemas-em-Python/1047.py
+++ Problemas-em-Python/1047.py
@@ -1,11 +1,3 @@
-"""Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
-Observação: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
-    Entrada
-        Quatro números inteiros representando a hora de início e fim do jogo.
-    Saída
-        Mostre a seguinte mensagem: 'O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)'."""
-
-
 def entrada():
     return map(int, input().split())
 
@@ -20,6 +12,7 @@ def horas_de_jogo(hi, mi, hf, mf):
     return horas, minutos
 
 
-hI, mI, hF, mF = entrada()
+hI, mI, hF, mF = map(int, input().split())
+houras = (24 + hF - ))
 hours, minutes = horas_de_jogo(hI, mI, hF, mF)
 print(f"O JOGO DUROU {hours} HORA(S) E {minutes} MINUTO(S)")
