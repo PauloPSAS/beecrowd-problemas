72e425baa4f4a79cc0de2f2221e6c976f4934663
Paulo <paulosergio.games1997@gmail.com>
2024-06-29 22:12:50 -0300

    correção no código
    não sei onde que errei, o cod anterior faz praticamente a mesma coisa, porém o beecrowd não aceita o primeiro exemplo

--- Problemas-em-Python/1168.py
+++ Problemas-em-Python/1168.py
@@ -1,13 +1,29 @@
 qtd = int(input())
 
-leds = (6, 2, 5, 5, 4, 5, 6, 3, 7, 6)
-
 for i in range(qtd):
     soma = 0
-    value = int(input())
+    value = input()
 
-    valueD = str(value)
-    for x in valueD:
-        soma += leds[int(x)]
-        
+    for x in value:
+        if x == '0':
+            soma += 6
+        elif x == '1':
+            soma += 2
+        elif x == '2':
+            soma += 5
+        elif x == '3':
+            soma += 5
+        elif x == '4':
+            soma += 4
+        elif x == '5':
+            soma += 5
+        elif x == '6':
+            soma += 6
+        elif x == '7':
+            soma += 3
+        elif x == '8':
+            soma += 7
+        elif x == '9':
+            soma += 6
+
     print(f"{soma} leds")
