13e254cb6461e70afddcc153044d8e729dc1f7af
Paulo <paulosergio.games1997@gmail.com>
2024-08-23 12:27:18 -0300

    desafio 1182
    Quase a mesma coisa do 1181, apenas mudando linha para coluna.

---
+++ Problemas-em-Python/1182.py
@@ -0,0 +1,32 @@
+def soma(c, m):
+    soma = 0
+    for linha in m:
+        soma += linha[c]
+    return soma
+
+def media(c, m):
+    media = cont = 0
+    for linha in m:
+            if linha[c] != 0:
+                 media += linha[c]
+                 cont += 1
+    return media / cont
+        
+
+
+matriz = [[0 for i in range(12)] for j in range(12)]
+
+C = int(input())
+
+caractere = input().upper()
+
+
+for lista in range(12):
+    for coluna in range(12):
+        matriz[lista][coluna] = float(input())
+
+if caractere == 'S':
+    print(f"{soma(C, matriz):.1f}")
+
+elif caractere == 'M':
+    print(f"{media(C, matriz):.1f}")
