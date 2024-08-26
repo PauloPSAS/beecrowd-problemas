7fd73b461cf5e8d240164b008d9c848cf1cb97ad
Paulo <paulosergio.games1997@gmail.com>
2024-08-25 21:06:42 -0300

    novo desafio

---
+++ Problemas-em-Python/1185.py
@@ -0,0 +1,33 @@
+def media_matriz(m):
+    media = cont = 0
+    for y in range(12):
+        for z in range(12):
+            if z < 11 - y:
+                media += m[y][z]
+                cont += 1
+    return media / cont if cont != 0 else 0
+
+
+def soma_matriz(m):
+    soma = 0
+    for y in range(12):
+        for z in range(12):
+            if z < 11 - y:
+                soma += m[y][z]
+    return soma
+
+
+matriz = [[0 for _ in range(12)] for _ in range(12)]
+
+O = input().upper()
+
+for i in range(12):
+    for j in range(12):
+        matriz[i][j] = float(input())
+
+if O == 'S':
+    s = soma_matriz(matriz)
+elif O == 'M':
+    s = media_matriz(matriz)
+
+print(f"{s:.1f}")
