32b80d9d2baa632992aa87f0d88c3a30d3c55ee4
Paulo <paulosergio.games1997@gmail.com>
2024-08-29 21:37:42 -0300

    1154

---
+++ Problemas-em-Python/1154.py
@@ -0,0 +1,13 @@
+def idade():
+    return int(input())
+
+
+i = idade()
+
+m = c = 0
+while i > 0:
+    if idade != 0:
+        m += i
+        c += 1
+    i = idade()
+print("{:.2f}".format(m / c))
