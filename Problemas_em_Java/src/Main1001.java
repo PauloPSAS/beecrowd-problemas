9169dc4c0a2116154eaffd36d2475e7380632a0f
Paulo <paulosergio.games1997@gmail.com>
2024-10-05 21:41:15 -0300

    .

---
+++ Problemas_em_Java/src/Main1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A;
+        int B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        System.out.printf("X = %d\n", A + B);
+    }
+}
