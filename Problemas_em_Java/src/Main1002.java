ce3c601b170638e67cca8179adb9faf868f89ff5
Paulo <paulosergio.games1997@gmail.com>
2024-10-07 18:45:48 -0300

    Create Main1002.java

---
+++ Problemas_em_Java/src/Main1002.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main1002 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double raio;
+        double pi = 3.14159;
+
+        raio = input.nextDouble();
+
+        System.out.printf("A=%.4f", pi * (raio * raio));
+    }
+
+}
