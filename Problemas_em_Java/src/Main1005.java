5cbae5d7c189412f453ecd976d6b8f18ea1cf428
Paulo <paulosergio.games1997@gmail.com>
2024-11-06 11:39:42 -0300

    Update Main1005.java

--- Problemas_em_Java/src/Main1005.java
+++ Problemas_em_Java/src/Main1005.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Main1005 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+
+        double media = ((3.5 * A) + (7.5 * B)) / (3.5 + 7.5);
+
+        System.out.printf("MEDIA = %.5f%n", media);
+    }
 }
