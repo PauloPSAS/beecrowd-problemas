f84cbe4954489e0ce33c47d4c5dd840cfe49891d
Paulo <paulosergio.games1997@gmail.com>
2024-11-06 11:30:34 -0300

    ...

---
+++ Problemas_em_Java/src/Main1003.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main1003 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        System.out.printf("SOMA = %d%n", A+B);
+    }
+}
---
+++ Problemas_em_Java/src/Main1004.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main1004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        System.out.printf("PROD = %d%n", A * B);
+    }
+}
