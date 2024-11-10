96def05ae9074484353854bd458f228ff369b991
Paulo <paulosergio.games1997@gmail.com>
2024-11-09 22:55:30 -0300

    Create Main1008.java

---
+++ Problemas_em_Java/src/Main1008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main1008 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+        int hoursWorked = input.nextInt();
+        double salaryHour = input.nextDouble();
+        double salaryFinal = salaryHour * hoursWorked;
+
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = U$ %.2f%n", salaryFinal);
+    }
+}
