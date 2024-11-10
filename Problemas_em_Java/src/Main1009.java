4fa4a14afdb56ca7ae99c57719c0ff2f933d9972
Paulo <paulosergio.games1997@gmail.com>
2024-11-09 23:06:02 -0300

    Create Main1009.java

---
+++ Problemas_em_Java/src/Main1009.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main1009 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+        double fixSalary = input.nextDouble();
+        double sales = input.nextDouble();
+        double salesCommission = sales / 100 * 15;
+        double totalSalary = fixSalary + salesCommission;
+
+        System.out.printf("TOTAL = R$ %.2f%n", totalSalary);
+    }
+}
