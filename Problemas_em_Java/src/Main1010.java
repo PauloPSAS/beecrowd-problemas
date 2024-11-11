93632099490a8a73e7a0e4a08303f241ded8d6ed
Paulo <paulosergio.games1997@gmail.com>
2024-11-10 23:04:57 -0300

    Create Main1010.java

---
+++ Problemas_em_Java/src/Main1010.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main1010 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int cod1 = input.nextInt();
+        int qtd1 = input.nextInt();
+        double value1 = input.nextDouble();
+
+        int cod2 = input.nextInt();
+        int qtd2 = input.nextInt();
+        double value2 = input.nextDouble();
+
+        double total = qtd1 * value1 + qtd2 * value2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+    }
+}
