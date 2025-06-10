98dac88de776695387cd4f0ef63bf120b9cdd717
Paulo <paulosergio.games1997@gmail.com>
2025-06-09 22:33:51 -0300

    1222.py
    Depois termino, deu preguiça

---
+++ Problemas-em-Python/1222.py
@@ -0,0 +1,12 @@
+import sys
+
+for caso in sys.stdin:
+    numPalavras, numLinhasPagina, numMaxCaracteresLinha = caso.split()
+    
+    numPalavras = int(numPalavras)
+    numLinhasPagina = int(numLinhasPagina)
+    numMaxCaracteresLinha = int(numMaxCaracteresLinha)
+
+    conto = input()
+
+    
