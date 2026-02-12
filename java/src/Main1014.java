4be48151ba6c105baad18cb268e972a3a324a624
Paulo <paulosergio.games1997@gmail.com>
2026-02-12 08:01:16 -0300

    desafios 1014 e 1015 adicionados em Java

---
+++ java/src/Main1014.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main1014 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        double y = input.nextDouble();
+
+        double distancia = x / y;
+
+        System.out.printf("%.3f km/l%n", distancia);
+    }
+}
